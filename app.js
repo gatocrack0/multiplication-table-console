9c5fbbdb5d1efdcad4b7f07af11223716a4cfd11
Vicente <vicente.barra.94@gmail.com>
2021-02-19 15:53:44 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const { crearArchivo } = require('./helpers/multiplicar')
+const argv = require('./config/yargs');
+const colors = require('colors');
+
+console.clear();
+
+// console.log( argv );
+// const base = "2";
+
+crearArchivo( argv.b, argv.l, argv.h)
+    .then( nombreArchivo => console.log( nombreArchivo.rainbow, 'creado') )
+    .catch( console.log );
+
+
